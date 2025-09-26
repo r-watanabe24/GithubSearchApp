fab27556adb2047dd30aed98011591552121c25b
l_0045_r.watanabe <r.watanabe@sonicmoov.com>
2025-09-26 18:24:14 +0900

    Initial commit

---
+++ GithubSearchApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubSearchApp
+//
+//  Created by L_0045_r.watanabe on 2025/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
